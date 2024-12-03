6404ea7d2f1d2ac6950d4fe55f1cb2337adc7025
SHU <shu@kimm.re.kr>
2024-12-03 09:01:44 +0000

    aa

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -12,10 +12,10 @@ from streamlit_authenticator.utilities import (CredentialsError,
                                                ResetError,
                                                UpdateError)
 
-klogo = "./asset/logo.png"
-sidebar_logo = klogo
+logo = "./asset/logo.png"
 
-st.title("산업부-AI스마트드라이버")
+
+st.title("2024연구행정혁신 - AI행정원 Demo")
 
 # 초기화
 if "authenticated" not in st.session_state:
@@ -33,55 +33,37 @@ if not st.session_state.authenticated:
             st.error("Access code is incorrect.")
 
 else:
-    st.title("test")
+    # Replicate Credentials
+    with st.sidebar:
+        st.title('AI행정원 @NST')
+
+        if 'openai' in st.secrets:
+            st.success('API key already provided!', icon='✅')
+            openai_api = st.secrets["api"]["openai"]
+        else:
+            openai_api = st.text_input("Enter OpenAI API Key", type="password")
+            if openai_api:
+                st.success('API key provided!', icon='✅')
+
+    # Store LLM generated responses
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+    # Display or clear chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 
-
-# # 인증 페이지
-# if not st.session_state.authenticated:
-#     st.title("산업부-AI스마트드라이버")
-#     password_input = st.text_input("Please enter the access code:", type="password")
-    # if st.button("Enter"):
-    #     if password_input == st.secrets["auth"]["password"]:
-    #         st.session_state.authenticated = True
-    #         # 페이지를 다시 로드하지 않고, 현재 상태에서 컨텐츠가 업데이트되도록 설정
-    #         st.rerun()
-    #     else:
-    #         st.error("Access code is incorrect.")
-# else:
-#     st.title("test")
-
-    # # Replicate Credentials
-    # with st.sidebar:
-    #     st.title('AI행정원 @NST')
-
-    #     if 'openai' in st.secrets:
-    #         st.success('API key already provided!', icon='✅')
-    #         openai_api = st.secrets["api"]["openai"]
-    #     else:
-    #         openai_api = st.text_input("Enter OpenAI API Key", type="password")
-    #         if openai_api:
-    #             st.success('API key provided!', icon='✅')
-
-    # # Store LLM generated responses
-    # if "messages" not in st.session_state.keys():
-    #     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-    # # Display or clear chat messages
-    # for message in st.session_state.messages:
-    #     with st.chat_message(message["role"]):
-    #         st.write(message["content"])
-
-    # def clear_chat_history():
-    #     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-    # st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
-
-    # # User-provided prompt
-    # if prompt := st.chat_input(disabled=not openai_api):  # `replicate_api` 대신 `openai_api` 사용
-    #     st.session_state.messages.append({"role": "user", "content": prompt})  # 역할 이름을 "user"로 변경
-    #     with st.chat_message("user"):  # 역할 이름에 맞게 "user"로 수정
-    #         st.write(prompt)
+    # User-provided prompt
+    if prompt := st.chat_input(disabled=not openai_api):  # `replicate_api` 대신 `openai_api` 사용
+        st.session_state.messages.append({"role": "user", "content": prompt})  # 역할 이름을 "user"로 변경
+        with st.chat_message("user"):  # 역할 이름에 맞게 "user"로 수정
+            st.write(prompt)
 
 
 # # Function for generating LLaMA2 response
