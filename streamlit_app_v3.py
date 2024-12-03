1fcd50eaf1b0f4b88407c6925971a1d2fe1699c5
SHU <shu@kimm.re.kr>
2024-12-03 09:12:24 +0000

    aa

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -58,6 +58,25 @@ else:
         st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
+    uploaded_file = st.sidebar.file_uploader(
+        label="연구계획서를 올려주세요",
+        type=["hwp", "hwpx"],  # 허용 파일 형식
+        accept_multiple_files=False  # 여러 파일 업로드 여부
+    )
+
+    # 업로드된 파일 처리
+    if uploaded_file:
+        st.sidebar.success("File uploaded successfully!")
+        # 파일 이름 표시
+        st.write(f"Uploaded file: {uploaded_file.name}")
+        
+        # # 파일 내용 읽기 (예: 텍스트 파일의 경우)
+        # try:
+        #     file_contents = uploaded_file.read().decode("utf-8")
+        #     st.text_area("File Content", file_contents)
+        # except Exception as e:
+        #     st.error("Unable to read file content.")
+
 
     # User-provided prompt
     if prompt := st.chat_input(disabled=not openai_api):  # `replicate_api` 대신 `openai_api` 사용
