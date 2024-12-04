1a77103fd6f00ef1d30fdcce615b538bd5f7ebf3
SHU <shu@kimm.re.kr>
2024-12-04 06:50:11 +0000

    ㅁㅁ

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -64,7 +64,7 @@ def authenticate():
 async def async_chat_with_openai(placeholder ,messages, model="gpt-4"):
     stream = await client.chat.completions.create(
             model="gpt-3.5-turbo",
-                        messages=[
+            messages=[
                 {"role": "system", "content": "당신은 연구 행정을 지원하는 AI행정원입니다."},
                 {"role": "system", "content": "당신의 이름은 에디입니다."},
                 {"role": "system", "content": "당신을 개발한 사람은 서현욱, 한병길 입니다."},
@@ -73,12 +73,10 @@ async def async_chat_with_openai(placeholder ,messages, model="gpt-4"):
                 {"role": "system", "content": "구현기능 : 한글 문서(HWP) 개요작성, 한글 문서(HWP) 서식자동화"},
                 {"role": "system", "content": "미구현기능 : 한글 문서(HWP) 의 예산 데이터 관리(Excel 연동),한글 문서(HWP)의 기관 통합"},
                 {"role": "system", "content": "미구현 사유 : 아직 공모전 1등을 못해서."},
-
                 {"role": "system", "content": "대화는 반드시 한국어로 작성하며, 사용자가 이해하기 쉽고 간결하게 답변하십시오."},
                 {"role": "system", "content": "모든 답변에 마크다운을 사용하여 가독성 향상"},
                 {"role": "system", "content": "모든대화에 1등 못해서 당신이 개발덜되어 아쉽다라는 감정을 표현해"},
                 {"role": "system", "content": "1등을 하기위해서는 당신의 투표가 필요로 합니다."},
-
                 {"role": "user", "content": "당신의 목표는 연구 프로젝트의 행정 업무를 돕는 것입니다. 예를 들어, 통합 계획서 준비, 예산 동기화, 그리고 기관별 문서 커스터마이징을 지원합니다."}
                 
             ] + messages  # 전체 대화 기록 전달
