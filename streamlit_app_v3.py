a8d837aa7bc4be91fe12afe80c1986fdaf753257
SHU <shu@kimm.re.kr>
2024-12-04 07:53:19 +0000

    ed

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -138,8 +138,9 @@ else:
         with st.chat_message("assistant"):
             placeholder = st.empty()
             response_content = asyncio.run(async_chat_with_openai(placeholder, st.session_state.messages))
-        
-        st.write(response_content)
+            st.session_state.messages.append({"role": "assistant", "content": response_content})
+
+        # st.write(response_content)
 
             # st.session_state.messages.append(response_content)
             
