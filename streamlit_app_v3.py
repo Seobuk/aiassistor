8676aea3e9b0a1f4d5f726455769a6224d1506aa
SHU <shu@kimm.re.kr>
2024-12-04 03:32:58 +0000

    a

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -117,18 +117,18 @@ else:
             st.write(prompt)
 
 
- # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+#  # Display assistant response in chat message container
+#     with st.chat_message("assistant"):
+#         stream = client.chat.completions.create(
+#             model=st.session_state["openai_model"],
+#             messages=[
+#                 {"role": m["role"], "content": m["content"]}
+#                 for m in st.session_state.messages
+#             ],
+#             stream=True,
+#         )
+#         response = st.write_stream(stream)
+#     st.session_state.messages.append({"role": "assistant", "content": response})
 
     # completion = client.chat.completions.create(
     #     model="gpt-4o-mini",
