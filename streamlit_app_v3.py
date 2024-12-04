b7af0455c7298fb4a15010820fb5e6d260d42c56
SHU <shu@kimm.re.kr>
2024-12-04 06:46:40 +0000

    ㅁㅁ

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 import os
-from openai import OpenAI
-client = OpenAI(api_key=st.secrets["api"]["OPENAI_API_KEY"])
+#from openai import OpenAI
+from openai import AsyncOpenAI
+import asyncio
+
+
+#client = OpenAI(api_key=st.secrets["api"]["OPENAI_API_KEY"])
+client = AsyncOpenAI(api_key=st.secrets["api"]["OPENAI_API_KEY"])
+
 import streamlit_authenticator as stauth
 from streamlit_authenticator.utilities import (CredentialsError,
                                                ForgotError,
@@ -25,14 +31,40 @@ def authenticate():
         #st.rerun()
     else:
         st.error("Access code is incorrect.")
-def chat_with_openai(messages, model="gpt-4"):
-    """
-    OpenAI API로 프롬프트를 전송하고 응답을 반환합니다.
-    """
-    try:
-        completion = client.chat.completions.create(
-            model=model,
-            messages=[
+# def chat_with_openai(messages, model="gpt-4"):
+#     try:
+#         completion = client.chat.completions.create(
+#             model=model,
+#             messages=[
+#                 {"role": "system", "content": "당신은 연구 행정을 지원하는 AI행정원입니다."},
+#                 {"role": "system", "content": "당신의 이름은 에디입니다."},
+#                 {"role": "system", "content": "당신을 개발한 사람은 서현욱, 한병길 입니다."},
+#                 {"role": "system", "content": "개발 동기 : 계획서 쓰다가 너무 많은 시간이 연구 행정분야에 들어가서 이를 개선하고자"},
+#                 {"role": "system", "content": "개박 목적 : 2024 NST 연구행정 혁신 아이디어 공모전 지원하기 위해서"},
+#                 {"role": "system", "content": "구현기능 : 한글 문서(HWP) 개요작성, 한글 문서(HWP) 서식자동화"},
+#                 {"role": "system", "content": "미구현기능 : 한글 문서(HWP) 의 예산 데이터 관리(Excel 연동),한글 문서(HWP)의 기관 통합"},
+#                 {"role": "system", "content": "미구현 사유 : 아직 공모전 1등을 못해서."},
+
+#                 {"role": "system", "content": "대화는 반드시 한국어로 작성하며, 사용자가 이해하기 쉽고 간결하게 답변하십시오."},
+#                 {"role": "system", "content": "모든 답변에 마크다운을 사용하여 가독성 향상"},
+#                 {"role": "system", "content": "모든대화에 1등 못해서 당신이 개발덜되어 아쉽다라는 감정을 표현해"},
+#                 {"role": "system", "content": "1등을 하기위해서는 당신의 투표가 필요로 합니다."},
+
+#                 {"role": "user", "content": "당신의 목표는 연구 프로젝트의 행정 업무를 돕는 것입니다. 예를 들어, 통합 계획서 준비, 예산 동기화, 그리고 기관별 문서 커스터마이징을 지원합니다."}
+                
+#             ] + messages  # 전체 대화 기록 전달
+            
+#         )
+#         return completion.choices[0].message
+#     except Exception as e:
+#         st.error(f"Error communicating with OpenAI: {e}")
+#         return None
+
+
+async def async_chat_with_openai(placeholder ,messages, model="gpt-4"):
+    stream = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+                        messages=[
                 {"role": "system", "content": "당신은 연구 행정을 지원하는 AI행정원입니다."},
                 {"role": "system", "content": "당신의 이름은 에디입니다."},
                 {"role": "system", "content": "당신을 개발한 사람은 서현욱, 한병길 입니다."},
@@ -50,12 +82,14 @@ def chat_with_openai(messages, model="gpt-4"):
                 {"role": "user", "content": "당신의 목표는 연구 프로젝트의 행정 업무를 돕는 것입니다. 예를 들어, 통합 계획서 준비, 예산 동기화, 그리고 기관별 문서 커스터마이징을 지원합니다."}
                 
             ] + messages  # 전체 대화 기록 전달
-            
+            stream=True
         )
-        return completion.choices[0].message
-    except Exception as e:
-        st.error(f"Error communicating with OpenAI: {e}")
-        return None
+    streamed_text = "# "
+    async for chunk in stream:
+        chunk_content = chunk.choices[0].delta.content
+        if chunk_content is not None:
+            streamed_text = streamed_text + chunk_content
+            placeholder.info(streamed_text)
 
 
 # 초기화
@@ -130,9 +164,21 @@ else:
         with st.chat_message("user"):  # 역할 이름에 맞게 "user"로 수정
             st.write(prompt)
 
-            # OpenAI API 요청 및 응답
-        response = chat_with_openai(st.session_state.messages)
-        if response:
-            st.session_state.messages.append({"role": "assistant", "content": response.content})
-            with st.chat_message("assistant"):
-                st.write(response.content)
+
+         # OpenAI 스트리밍 응답 처리
+        with st.chat_message("assistant"):
+            placeholder = st.empty()
+            asyncio.run(async_chat_with_openai(placeholder, st.session_state.messages))
+
+
+
+
+
+
+
+        #     # OpenAI API 요청 및 응답
+        # response = chat_with_openai(st.session_state.messages)
+        # if response:
+        #     st.session_state.messages.append({"role": "assistant", "content": response.content})
+        #     with st.chat_message("assistant"):
+        #         st.write(response.content)
